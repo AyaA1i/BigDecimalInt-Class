e47948ee0f1d0a4a2c46d73340d918b9a9aabd31
Aya Ali <101468593+ayaaalli@users.noreply.github.com>
2022-12-05 23:12:20 +0200

    Add files via upload

---
+++ BigDecimalIntClass.h
@@ -0,0 +1,35 @@
+#ifndef BIGDECIMALINT_BIGDECIMALINTCLASS_H
+#define BIGDECIMALINT_BIGDECIMALINTCLASS_H
+
+#include <iostream>
+#include <deque>
+#include <regex>
+
+using namespace std;
+
+class BigDecimalInt{
+private:
+    string number;
+    char sign;
+    void setNumber(string num);
+    bool checkValidInput(string input);
+
+public:
+    bool operator < (const BigDecimalInt& anotherDec);
+    bool operator > (const BigDecimalInt& anotherDec);
+    bool operator == (const BigDecimalInt anotherDec);
+    BigDecimalInt& operator = (BigDecimalInt &anotherDec);
+    BigDecimalInt operator + (BigDecimalInt number2);
+    BigDecimalInt operator - (BigDecimalInt anotherDec);
+    friend ostream &operator << (ostream &out, BigDecimalInt num);
+    int size();
+    int Sign();
+
+    BigDecimalInt(){}
+    BigDecimalInt(string num)
+    {
+        setNumber(num);
+    }
+};
+
+#endif //BIGDECIMALINT_BIGDECIMALINTCLASS_H
